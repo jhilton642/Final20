ff9bc88217744370b4d2ca447bc6f4d9ea20357d
jhilton642 <joshua.hilton@att.net>
2020-10-26 14:22:28 -0500

    Add project files.

---
+++ Final20/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Final20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What year where you born?: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What month? (1-12) : ");
+            int month = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What day?: ");
+            int day = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your age in seconds is " + Program.CalculateAge(year,month,day));
+            
+        }
+        public static double CalculateAge(int year, int month, int day)
+        {
+            double ageInSeconds;
+            DateTime now = DateTime.Now;
+            DateTime dob = new DateTime(year, month, day);
+            ageInSeconds = (now - dob).TotalSeconds;
+            return ageInSeconds;
+        }
+    }
+}
